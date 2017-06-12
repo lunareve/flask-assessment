ba07fa5c432a67802a9026d270beb79d9ed240c7
Thomas Nocon <NetwrkNinja@Thomass-MacBook-Air.local>
2017-06-11 19:37:24 -0700

    add part1 functionality

--- server.py
+++ server.py
@@ -10,14 +10,37 @@ app.jinja_env.auto_reload = True
 # Required to use Flask sessions and the debug toolbar
 app.secret_key = "ABC"
 
+OPEN_JOBS = ["Software Engineer", "QA Engineer", "Product Manager"]
 
 @app.route('/')
 def homepage():
+	"""Renders the homepage."""
+
 	return render_template("index.html")
 
-@app.route('/application-form')
+
+@app.route('/application-form', methods=["POST"])
 def application-form():
-	return render_template("application-form.html")
+	"""Allows user to apply for open jobs."""
+
+	return render_template("application-form.html",
+							OPEN_JOBS=OPEN_JOBS)
+
+@app.route('/application-success', methods=["POST"])
+def success():
+	"""Confirms that user applied to specified job."""
+
+	first_name = request.form.get("first-name")
+	last_name = request.form.get("last-name")
+	salary = request.form.get("salary")
+	position = request.form.get("position")
+
+	return render_template("application-response.html",
+							first_name=first_name,
+							last_name=last_name,
+							salary=salary,
+							position=position)
+
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
